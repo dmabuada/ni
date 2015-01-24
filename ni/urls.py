6c31202456c1b04be0b61ade014a1ec6fa59b7d3
jsvk <jsvk91+github@gmail.com>
2015-01-24 15:28:38 -0500

    Test ci

--- ni/urls.py
+++ ni/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.http import HttpResponse as GetRidOfMe
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ni.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/', lambda req: GetRidOfMe('x'))
 )
