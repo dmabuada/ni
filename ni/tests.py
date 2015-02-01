476596ca4f898b5aadd77d3a66f00ce54beae049
dalia <abuadas.dalia@gmail.com>
2015-02-01 16:00:14 -0500

    Add tests for ping route

--- ni/tests.py
+++ ni/tests.py
@@ -4,6 +4,13 @@ from django.test import TestCase
 class HomePageTestCase(TestCase):
 
     def test_index(self):
+        """Assert that the homepage works"""
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 302)
         self.assertTrue(resp.url.endswith('/en-us/'))
+
+    def test_ping(self):
+        """Assert that the ping route comes back with a 200"""
+        resp = self.client.get('/ping')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.content)
