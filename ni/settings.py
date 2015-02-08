e39f648c84346217917861be5b1a66bcbfc1d5b8
dalia <abuadas.dalia@gmail.com>
2015-02-08 14:06:15 -0500

    Switch to satchmo from oscar

--- ni/settings.py
+++ ni/settings.py
@@ -1,400 +1,173 @@
+# Django settings for satchmo project.
+# This is a recommended base setting for further customization, default for clonesatchmo.py
 import os
 
-# Path helper
-location = lambda x: os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), x)
+DIRNAME = os.path.dirname(os.path.normpath(os.path.abspath(__file__)))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DJANGO_PROJECT = 'store'
+DJANGO_SETTINGS_MODULE = 'store.settings'
 
+ADMINS = (
+     ('', ''),         # tuple (name, email) - important for error reports sending, if DEBUG is disabled.
+)
 
-USE_TZ = True
+MANAGERS = ADMINS
 
-DEBUG = True
-TEMPLATE_DEBUG = True
-SQL_DEBUG = True
-
-EMAIL_SUBJECT_PREFIX = '[Oscar US sandbox] '
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Use a Sqlite database by default
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-        'ATOMIC_REQUESTS': True
-    }
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
-
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/New_York'
+# Local time zone for this installation. All choices can be found here:
+# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+TIME_ZONE = 'US/Pacific'
 
 # Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
 LANGUAGE_CODE = 'en-us'
-
-# Includes all languages that have >50% coverage in Transifex
-# Taken from Django's default setting for LANGUAGES
-gettext_noop = lambda s: s
-LANGUAGES = (
-    ('en-us', gettext_noop('American English')),
-)
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = location("public/media")
+# Image files will be stored off of this path
+#
+# If you are using Windows, recommend using normalize_path() here
+#
+# from satchmo_utils.thumbnail import normalize_path
+# MEDIA_ROOT = normalize_path(os.path.join(DIRNAME, 'static/'))
+MEDIA_ROOT = os.path.join(DIRNAME, 'static/')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+MEDIA_URL="/static/"
+
+# STATIC_ROOT can be whatever different from other dirs
+STATIC_ROOT = os.path.join(DIRNAME, 'static-collect/')
+STATIC_URL = '/static-collect/'
+
+STATICFILES_DIRS = (
+        os.path.join(DIRNAME, 'static/'),
+)
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-#ADMIN_MEDIA_PREFIX = '/media/admin/'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-STATICFILES_DIRS = (
-    # 'static',
-)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
+ADMIN_MEDIA_PREFIX = '/media/'  # remove for Django 1.4 as deprecated
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '$)a6n&o80u!6y5t-+jrd3)3!%vh&shg$wqpjpxc!ar&p#!)n1a'
+SECRET_KEY = '&jrnmg5)=ja@ioe1vhy51^j0hupv#!6nn4hr_poqs6go_iixkz'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    # needed by django-treebeard for admin (and potentially other libs)
-    'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.request",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.contrib.messages.context_processors.messages",
-    # Oscar specific
-    'oscar.apps.search.context_processors.search_form',
-    'oscar.apps.promotions.context_processors.promotions',
-    'oscar.apps.checkout.context_processors.checkout',
-    'oscar.core.context_processors.metadata',
-    'oscar.apps.customer.notifications.context_processors.notifications',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    # Allow languages to be selected
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    # Ensure a valid basket is added to the request instance for every request
-    'oscar.apps.basket.middleware.BasketMiddleware',
-    # Enable the ProfileMiddleware, then add ?cprofile to any
-    # URL path to print out profile details
-    # 'oscar.profiling.middleware.ProfileMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.doc.XViewMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "threaded_multihost.middleware.ThreadLocalMiddleware",
+    "satchmo_store.shop.SSLMiddleware.SSLRedirect",
+    #"satchmo_ext.recentlist.middleware.RecentProductMiddleware",
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+#this is used to add additional config variables to each request
+# NOTE: If you enable the recent_products context_processor, you MUST have the
+# 'satchmo_ext.recentlist' app installed.
+TEMPLATE_CONTEXT_PROCESSORS = (
+        'satchmo_store.shop.context_processors.settings',
+        'django.contrib.auth.context_processors.auth',
+        #'satchmo_ext.recentlist.context_processors.recent_products',
+        # do not forget following. Maybe not so important currently
+        # but will be
+        'django.core.context_processors.media',   # MEDIA_URL
+        'django.core.context_processors.static',  # STATIC_URL
+        'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'ni.urls'
 
-# Add another path to Oscar's templates.  This allows templates to be
-# customised easily.
-from oscar import OSCAR_MAIN_TEMPLATE_DIR
 TEMPLATE_DIRS = (
-    location('templates'),
-    OSCAR_MAIN_TEMPLATE_DIR,
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIRNAME,'templates'),
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(message)s',
-        },
-        'simple': {
-            'format': '[%(asctime)s] %(message)s'
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'django.utils.log.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-        'checkout_file': {
-            'level': 'INFO',
-            'class': 'oscar.core.logging.handlers.EnvFileHandler',
-            'filename': 'checkout.log',
-            'formatter': 'verbose'
-        },
-        'gateway_file': {
-            'level': 'INFO',
-            'class': 'oscar.core.logging.handlers.EnvFileHandler',
-            'filename': 'gateway.log',
-            'formatter': 'simple'
-        },
-        'error_file': {
-            'level': 'INFO',
-            'class': 'oscar.core.logging.handlers.EnvFileHandler',
-            'filename': 'errors.log',
-            'formatter': 'verbose'
-        },
-        'sorl_file': {
-            'level': 'INFO',
-            'class': 'oscar.core.logging.handlers.EnvFileHandler',
-            'filename': 'sorl.log',
-            'formatter': 'verbose'
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['require_debug_false'],
-        },
-    },
-    'loggers': {
-        # Django loggers
-        'django': {
-            'handlers': ['null'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'django.request': {
-            'handlers': ['mail_admins', 'error_file'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'django.db.backends': {
-            'handlers': ['null'],
-            'propagate': False,
-            'level': 'DEBUG',
-        },
-        # Oscar core loggers
-        'oscar.checkout': {
-            'handlers': ['console', 'checkout_file'],
-            'propagate': False,
-            'level': 'INFO',
-        },
-        'oscar.catalogue.import': {
-            'handlers': ['console'],
-            'propagate': False,
-            'level': 'INFO',
-        },
-        'oscar.alerts': {
-            'handlers': ['null'],
-            'propagate': False,
-            'level': 'INFO',
-        },
-        # Sandbox logging
-        'gateway': {
-            'handlers': ['gateway_file'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        # Third party
-        'south': {
-            'handlers': ['null'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'sorl.thumbnail': {
-            'handlers': ['sorl_file'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        # Suppress output of this debug toolbar panel
-        'template_timings_panel': {
-            'handlers': ['null'],
-            'level': 'DEBUG',
-            'propagate': False,
-        }
-    }
-}
-
-
-INSTALLED_APPS = [
+INSTALLED_APPS = (
+    'django.contrib.sites',
+    'satchmo_store.shop',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.comments',
     'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.admin',
-    'django.contrib.flatpages',
-    'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'django_extensions',
-    # Debug toolbar + extensions
-    'debug_toolbar',
-    'template_timings_panel',
-    'compressor',       # Oscar's templates use compressor
-    'widget_tweaks',
-]
-from oscar import get_core_apps
-INSTALLED_APPS = INSTALLED_APPS + get_core_apps(
-    ['ni.apps.partner', 'ni.apps.checkout', 'ni.apps.shipping'])
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'registration',
+    'sorl.thumbnail',
+    'keyedcache',
+    'livesettings',
+    'l10n',
+    'satchmo_utils.thumbnail',
+    'satchmo_store.contact',
+    'tax',
+    'tax.modules.no',
+    'tax.modules.area',
+    'tax.modules.percent',
+    'shipping',
+    #'satchmo_store.contact.supplier',
+    #'shipping.modules.tiered',
+    #'satchmo_ext.newsletter',
+    #'satchmo_ext.recentlist',
+    'product',
+    'product.modules.configurable',
+    'product.modules.custom',
+    #'product.modules.downloadable',
+    #'product.modules.subscription',
+    #'satchmo_ext.product_feeds',
+    #'satchmo_ext.brand',
+    'payment',
+    'payment.modules.dummy',
+    #'payment.modules.purchaseorder',
+    #'payment.modules.giftcertificate',
+    #'satchmo_ext.wishlist',
+    #'satchmo_ext.upsell',
+    #'satchmo_ext.productratings',
+    'satchmo_ext.satchmo_toolbar',
+    'satchmo_utils',
+    #'shipping.modules.tieredquantity',
+    #'satchmo_ext.tieredpricing',
+    #'debug_toolbar',
+    'app_plugins',
+    'ni.localsite',
+)
 
-import django
-if django.VERSION < (1, 7):
-    INSTALLED_APPS.append('south')
-
-# Add Oscar's custom auth backend so users can sign in using their email
-# address.
 AUTHENTICATION_BACKENDS = (
-    'oscar.apps.customer.auth_backends.EmailBackend',
+    'satchmo_store.accounts.email-auth.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
-LOGIN_REDIRECT_URL = '/'
-APPEND_SLASH = True
+#DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS' : False,
+#}
 
-# Haystack settings
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
-    }
+#### Satchmo unique variables ####
+#from django.conf.urls import patterns, include
+SATCHMO_SETTINGS = {
+    'SHOP_BASE' : '',
+    'MULTISHOP' : False,
+    'DOCUMENT_CONVERTER': 'shipping.views.HTMLDocument',
+    #'SHOP_URLS' : patterns('satchmo_store.shop.views',)
 }
 
-# =============
-# Debug Toolbar
-# =============
+SKIP_SOUTH_TESTS=True
 
-# Implicit setup can often lead to problems with circular imports, so we
-# explicitly wire up the toolbar
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-]
-INTERNAL_IPS = ['127.0.0.1', '::1']
-
-# ==============
-# Oscar settings
-# ==============
-
-from oscar.defaults import *  # noqa
-
-# Meta
-# ====
-
-OSCAR_SHOP_TAGLINE = 'US Sandbox'
-OSCAR_DEFAULT_CURRENCY = 'USD'
-OSCAR_ALLOW_ANON_CHECKOUT = True
-
-# LESS/CSS/statics
-# ================
-
-# We default to using CSS files, rather than the LESS files that generate them.
-# If you want to develop Oscar's CSS, then set USE_LESS=True and
-# COMPRESS_ENABLED=False in your settings_local module and ensure you have
-# 'lessc' installed.
-USE_LESS = False
-
-COMPRESS_ENABLED = True
-COMPRESS_PRECOMPILERS = (
-    ('text/less', 'lessc {infile} {outfile}'),
-)
-COMPRESS_OFFLINE_CONTEXT = {
-    'STATIC_URL': 'STATIC_URL',
-    'use_less': USE_LESS,
-}
-
-# We do this to work around an issue in compressor where the LESS files are
-# compiled but compression isn't enabled.  When this happens, the relative URL
-# is wrong between the generated CSS file and other assets:
-# https://github.com/jezdez/django_compressor/issues/226
-COMPRESS_OUTPUT_DIR = 'oscar'
-
-# Logging
-# =======
-
-LOG_ROOT = location('logs')
-# Ensure log root exists
-if not os.path.exists(LOG_ROOT):
-    os.mkdir(LOG_ROOT)
-
-# Sorl
-# ====
-
-THUMBNAIL_DEBUG = True
-THUMBNAIL_KEY_PREFIX = 'oscar-us-sandbox'
-
-# Django 1.6 has switched to JSON serializing for security reasons, but it does
-# not serialize Models. We should resolve this by extending the
-# django/core/serializers/json.Serializer to have the `dumps` function. Also in
-# tests/config.py
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-# Try and import local settings which can be used to override any of the above.
-try:
-    from settings_local import *  # noqa
-except ImportError:
-    pass
+# Load the local settings
+from local_settings import *
