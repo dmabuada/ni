85462bc6077fac5f761630d35b9ebd8ed00fd921
jsvk <jsvk91+github@gmail.com>
2015-02-14 20:51:34 -0500

    Pull satchmo in as a local dependency for better management

---
+++ satchmo/satchmo_utils/thumbnail/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SatchmoThumbnailConfig(AppConfig):
+    name = 'satchmo.satchmo_utils.thumbnail'
+    verbose_name = "satchmo thumbnail"
+    label = 'satchmo.thumbnails'
