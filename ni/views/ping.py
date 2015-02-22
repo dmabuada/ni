c44b9044e175989d56c69ac55f157018b6a0b33f
dalia <abuadas.dalia@gmail.com>
2015-02-22 15:07:52 -0500

    fixing our ping message..because..yeah

--- ni/views/ping.py
+++ ni/views/ping.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def ping(request):
     response_data = {}
-    response_data['result'] = 'dogs are always'
-    response_data['message'] = 'in the pushup position'
+    response_data['result'] = 'in the pushup position'
+    response_data['message'] = 'dogs are always'
     return HttpResponse(json.dumps(response_data), content_type="application/json")
