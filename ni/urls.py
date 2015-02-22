6929f6248d25a173493960d8eb34f4864a668f2d
jsvk <jsvk91+github@gmail.com>
2015-02-22 15:38:40 -0500

    Fix url reverse lookup in templates, name ping route

--- ni/urls.py
+++ ni/urls.py
@@ -6,16 +6,15 @@ from ni import views
 
 urlpatterns += patterns('',
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^ping/', views.ping)
+    url(r'^ping/', views.ping, name='health')
 )
 
 # if settings.DEBUG:
 # import debug_toolbar
-# 
+#
 # # Server statics and uploaded media
 #     urlpatterns += static(settings.MEDIA_URL,
 #                           document_root=settings.MEDIA_ROOT)
 #     urlpatterns += [
 #         url(r'^__debug__/', include(debug_toolbar.urls)),
 #     ]
-
