8b22e92317a67e4bd11a18eb2d05caf53f525d9a
dalia <abuadas.dalia@gmail.com>
2015-02-22 14:59:42 -0500

    apparently we need a message

--- ni/views/ping.py
+++ ni/views/ping.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def ping(request):
     response_data = {}
-    response_data['result'] = 'test'
-    response_data['message'] = 'test'
+    response_data['result'] = 'dogs are always'
+    response_data['message'] = 'in the pushup position'
     return HttpResponse(json.dumps(response_data), content_type="application/json")
