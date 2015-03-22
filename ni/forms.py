0491049850184f2956d0812a95a80d9685fff484
jsvk <jsvk91+github@gmail.com>
2015-03-22 17:07:39 -0400

    Add size to search form

--- ni/forms.py
+++ ni/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from product.models import Category
+from product.models import Option
 
 
 class SearchForm(forms.Form):
     k = forms.CharField(label='', max_length=100, required=False)
     category = forms.ModelChoiceField(Category.objects.all(), required=False)
+
+    size = forms.ModelChoiceField(
+        Option.objects.values_list('name', flat=True).distinct(),
+        required=False
+    )
