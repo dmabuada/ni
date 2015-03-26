6aab25be11845427a4c0bea2f659c674c0477c63
jsvk <jsvk91+github@gmail.com>
2015-03-26 01:08:50 -0400

    Set up form object for search page, accept and filter on keyword and category input

--- ni/forms.py
+++ ni/forms.py
@@ -3,9 +3,30 @@ from product.models import Category
 from product.models import Option
 
 
+# TODO: use the one from the homepage correctly
+from django.forms.extras.widgets import SelectDateWidget
+
+
+# class DateRangeSelectWidget(forms.TextInput):
+#     def render(self, name, value, attrs=None):
+#         return super(DateRangeSelectWidget, self).render(
+#             self, name, value, attrs
+#         )
+
+
 class SearchForm(forms.Form):
     k = forms.CharField(label='', max_length=100, required=False)
     category = forms.ModelChoiceField(Category.objects.all(), required=False)
+
+    start_date = forms.CharField(
+        widget=forms.TextInput(attrs={'id': 'delivery-date'}),
+        required=False
+    )
+
+    end_date = forms.CharField(
+        widget=forms.TextInput({'id': 'return-date'}),
+        required=False
+    )
 
     size = forms.ModelChoiceField(
         Option.objects.values_list('name', flat=True).distinct(),
--- ni/listeners.py
+++ ni/listeners.py
@@ -1,5 +1,6 @@
 from product.models import Category
-from product.models import Product
+from product.models import Product, OptionGroup
+from product.modules.configurable.models import ProductVariation
 from django.db.models import Q
 from django.contrib.sites.models import Site
 from livesettings import config_value
@@ -17,15 +18,8 @@ def product_search_listener(sender, query, **kwargs):
     signals
     """
 
-    print 'fuck'
-
-    x = Option.objects.distinct('name')
-
-    category = query.get('category', None)
-    if category:
-        category = category.slug
-
     keywords = query.get('k', '').split()
+    size = query.get('size', None)
 
     log.debug('default product search listener')
     site = Site.objects.get_current()
@@ -33,8 +27,10 @@ def product_search_listener(sender, query, **kwargs):
     show_pv = config_value('PRODUCT', 'SEARCH_SHOW_PRODUCTVARIATIONS', False)
 
     products = Product.objects.active_by_site(variations=show_pv, site=site)
+
+    category = query.get('category', None)
     if category:
-        categories = Category.objects.active(site=site, slug=category)
+        categories = Category.objects.active(site=site, slug=category.slug)
         if categories:
             categories = categories[0].get_active_children(include_self=True)
         products = products.filter(category__in=categories)
@@ -60,6 +56,13 @@ def product_search_listener(sender, query, **kwargs):
             | Q(meta__icontains=keyword)
             | Q(sku__iexact=keyword)
         )
+
+    if size:
+        # size_options = OptionGroup.objects.filter(name='size')
+        # products = products.filter(
+        #     Q(option_group__name='size')
+        # )
+        pass
 
     return {
         'categories': categories,
