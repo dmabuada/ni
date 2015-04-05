0099fe83c89f1b744ade3aa2546ca6dfbba709d6
jsvk <jsvk91+github@gmail.com>
2015-04-05 17:14:44 -0400

    Enable productratings in ci_settings.py

--- ni/ci_settings.py
+++ ni/ci_settings.py
@@ -154,7 +154,7 @@ INSTALLED_APPS = (
     # 'payment.modules.giftcertificate',
     #'satchmo_ext.wishlist',
     #'satchmo_ext.upsell',
-    #'satchmo_ext.productratings',
+    'satchmo_ext.productratings',
     'satchmo_ext.satchmo_toolbar',
     'satchmo_utils',
     #'shipping.modules.tieredquantity',
