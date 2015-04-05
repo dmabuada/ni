d8238831f6fb53ef95b48d5506595c480783f1c5
jsvk <jsvk91+github@gmail.com>
2015-04-05 15:09:53 -0400

    Add more test placeholders

--- ni/tests/test_search.py
+++ ni/tests/test_search.py
@@ -2,9 +2,17 @@ from django.test import TestCase
 
 class SearchTestCase(TestCase):
 
-    def test_search(self):
-        """Assert that search correctly filters comes back with a 200"""
+    def test_search_page(self):
+        """Assert that the search page comes back with a 200"""
         resp = self.client.get('/search/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.content)
+
+    def test_filtering_size(self):
+        """Assert that filtering on a size actually works"""
+        pass
+
+    def test_filtering_keywords(self):
+        """Assert that filtering by keyword actually works"""
+        pass
 
