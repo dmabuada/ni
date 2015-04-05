f3ed2130f82e38b49a289ac4c895e63c3f72a186
dalia <abuadas.dalia@gmail.com>
2015-04-05 14:53:52 -0400

    reset password now opens in modal

--- ni/static/js/login.js
+++ ni/static/js/login.js
@@ -1,16 +1,19 @@
+/** TODO: clean up this shitty code but get it to work for now*/
+
+
 $(document).ready(function() {
 $('a.login-window').click(function() {
     
-  var loginBox = $(this).attr('href');
+  var popModal = $(this).attr('href');
 
   //fade in on click
-  $(loginBox).fadeIn(200);
+  $(popModal).fadeIn(200);
   
   //center alignment padding + border 
-  var popMargTop = ($(loginBox).height() + 24) / 2;
-  var popMargLeft = ($(loginBox).width() + 24) / 2;
+    popMargTop = ($(popModal).height() + 24) / 2;
+    popMargLeft = ($(popModal).width() + 24) / 2;
   
-  $(loginBox).css({
+  $(popModal).css({
       'margin-top' : -popMargTop,
       'margin-left' : -popMargLeft
   });
@@ -18,16 +21,26 @@ $('a.login-window').click(function() {
   // append overlay to body
   $('body').append('<div id="overlay"></div>');
   $('#overlay').fadeIn(300);
+
+  // open forgot password in same modal 
+  $('a.forgot-window').click(function() {
+      $('#login-box').remove();
+
+      var forgotModal = $(this).attr('href');
+
+      $(forgotModal).fadeIn(200);
+  });
+    return false;
+  });
   
   return false;
 });
 
 $(function() {
   $(document).on('click','a.close, #overlay',function() {
-    $('#overlay,.login-popup').fadeOut('300',function() {
+    $('#overlay, #login-box').fadeOut('300',function() {
     $('#overlay').remove();
   });
     return false;
   });
   });
-});
